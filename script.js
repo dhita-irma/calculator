03331ed078d063b0e03a8111b5665d3f41f95b1e
dhita-irma <52138969+dhita-irma@users.noreply.github.com>
2023-04-20 19:23:00 -0700

    Create function operate()

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function add(a, b) {
-    return a + b 
-}
+function add(num1, num2) {
+    return num1 + num2;
+  }
+  
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
+  
+  function divide(num1, num2) {
+    if (num2 === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return num1 / num2;
+  }
 
-function subtract(a, b) {
-    return a - b 
-}
-
-function multiply(a, b) {
-    return a * b 
-}
-
-function divide(a, b) {
-    return a / b 
-}
+function operate(operator, num1, num2) {
+    switch (operator) {
+      case "+":
+        return add(num1, num2);
+      case "-":
+        return subtract(num1, num2);
+      case "*":
+        return multiply(num1, num2);
+      case "/":
+        return divide(num1, num2);
+      default:
+        throw new Error(`Unknown operator: ${operator}`);
+    }
+  }
+  
